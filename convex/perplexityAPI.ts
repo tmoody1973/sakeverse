3d0e7ae8efd2941e70dc9767de17cb29e5e20df5
Tarik Moody <tarikjmoody@gmail.com>
2026-01-04 23:44:43 -0600

    fix: Update Perplexity model from llama-3.1-sonar-small-128k-online to sonar

--- convex/perplexityAPI.ts
+++ convex/perplexityAPI.ts
@@ -35,7 +35,7 @@ export const searchWebContent = action({
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "llama-3.1-sonar-small-128k-online",
+          model: "sonar",
           messages: [
             {
               role: "system",
